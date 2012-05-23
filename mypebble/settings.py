8cedc523590b965ad93ebdfec6bf3f54e271ac83
ah <devnull@localhost>
2012-05-23 09:30:37 +0100

    Setup system

--- mypebble/settings.py
+++ mypebble/settings.py
@@ -19,7 +19,7 @@ DATABASES = {
         'NAME': 'mypebble_cms',                      # Or path to database file if using sqlite3.
         'USER': 'pebble',                      # Not used with sqlite3.
         'PASSWORD': 'pebble',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
