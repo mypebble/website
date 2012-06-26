cf2c104e962ab2c43a046459e2bf62ae3d922a44
Adam Hogg <ah@talktopebble.co.uk>
2012-06-26 16:54:23 +0100

    Post to getsatisfaction

--- mypebble/settings.py
+++ mypebble/settings.py
@@ -16,12 +16,12 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'mypebble_cms',                      # Or path to database file if using sqlite3.
-        'USER': 'pebble',                     	     # Not used with sqlite3.
-        'PASSWORD': 'pebble',                  # Not used with sqlite3.
-        'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',# Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'mypebble_cms',                            # Or path to database file if using sqlite3.
+        'USER': 'pebble',                     	           # Not used with sqlite3.
+        'PASSWORD': 'pebble',                              # Not used with sqlite3.
+        'HOST': 'localhost',                               # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                                        # Set to empty string for default. Not used with sqlite3.
     }
 }
 
