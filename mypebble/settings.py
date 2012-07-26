31d46fa01460b9e6c13c3289d0639f7b23047f7f
Adam Hogg <ah@talktopebble.co.uk>
2012-07-26 08:52:55 +0100

    static update

--- mypebble/settings.py
+++ mypebble/settings.py
@@ -123,6 +123,7 @@ TEMPLATE_DIRS = (
   # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
     os.path.join(PROJECT_PATH, 'templates'),
+    os.path.join(PROJECT_PATH, 'templates/forms'),
 )
 
 TEMPLATE_CONTEXT_PROCESSORS = (
