4bc2702d8224a14c33c2931b4e55a7509158978f
mk <devnull@localhost>
2012-08-29 16:34:21 +0100

    Added 'read more' link for testimonials on home page, custom view.

--- mypebble/testimonials/views.py
+++ mypebble/testimonials/views.py
@@ -1,8 +1,13 @@
 # mypebble/testimonials/views.py
 # MK: 29-Aug-2012
 
-from django.views.generic import TemplateView
+#from django.views.generic import TemplateView
+from django.views.generic import DetailView
+
+from cmsplugin_randomquote.models import Quote
 
 
-class TestimonialsView(TemplateView):
-    template_name = "testimonials.html"
+class TestimonialView(DetailView):
+    template_name = "testimonial.html"
+    model = Quote
+    context_object_name = 'testimonial'
