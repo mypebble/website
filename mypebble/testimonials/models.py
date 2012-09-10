9879565ee498f4aea1d69b7378449dab56bff954
mk <devnull@localhost>
2012-09-10 12:15:42 +0100

    Extended third party quotes model to allow preview testimonial text. WWW-24, WWW-25, WWW-26, WWW-27.

--- mypebble/testimonials/models.py
+++ mypebble/testimonials/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+from cmsplugin_randomquote.models import Quote
+
+class Testimonial(Quote):
+    quote_text_full = models.CharField(max_length=500)
+
