4007c3242e0a4e53d769763708fcceffd18663c9
scw <devnull@localhost>
2012-09-11 09:11:53 +0100

    Skipping unnecessary test

--- mypebble/core/tests.py
+++ mypebble/core/tests.py
@@ -1,10 +1,12 @@
 """Use the database values from initial_data for the page structure. This is
 mostly sanity tests to ensure the website loads the correct pages.
 """
+from unittest import skip
 
 from django.test import TestCase, client
 
 
+@skip('Part of Django CMS, so not really necessary')
 class SimplePageTestCase(TestCase):
 
     fixtures = [
