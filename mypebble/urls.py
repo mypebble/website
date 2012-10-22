002f76993bd3cf61a148fa6078af67e45824b8e4
Adam Hogg <ah@talktopebble.co.uk>
2012-10-22 12:29:14 +0100

    urls fix and started work on conditional statement

--- mypebble/urls.py
+++ mypebble/urls.py
@@ -32,7 +32,8 @@ if settings.DEBUG:
     urlpatterns = patterns('',
         url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
             {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
-
+        url(r'^static/(?P<path>.*)$', 'django.views.static.serve',
+            {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
 
     url(r'^admin/', include(admin.site.urls)),
 
