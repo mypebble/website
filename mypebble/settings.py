ff569c34e1e3ee4defb35bd4630ffb84b7490579
Adam Hogg <ah@talktopebble.co.uk>
2013-01-04 12:06:43 +0000

    updated responsive css to remove feedback tab

--- mypebble/settings.py
+++ mypebble/settings.py
@@ -272,10 +272,5 @@ CMS_TEMPLATES = (
     #('cmsplugin_blog_base.html', 'cmsplugin_blog'),
 )
 
-#for blog
-#JQUERY_JS = 'https://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js'
-#JQUERY_UI_JS = 'https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.12/jquery-ui.min.js'
-#JQUERY_UI_CSS = 'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.12/themes/smoothness/jquery-ui.css'
-
 #SEO
 CMS_SEO_FIELDS = True
