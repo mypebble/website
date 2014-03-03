df4ddb081598cfe41ef620cf1c723b2abec37088
Scott Walton <scott.walton@mypebble.co.uk>
2014-03-03 10:49:24 +0000

    Create the Testimonials in setUp() to bypass the bizarre fixture issue

--- mypebble/testimonials/tests.py
+++ mypebble/testimonials/tests.py
@@ -1,19 +1,35 @@
 from django.test import TestCase
 from django.core.urlresolvers import reverse
 
+from mypebble.testimonials.models import Testimonial
+
 
 class TestimonialTestCase(TestCase):
     """Confirms that testimonials are correctly displayed in extended form."""
 
-    fixtures = [
-        'testimonials/testimonials.json',
-    ]
+    def setUp(self):
+        """Manually loading the fixtures because CircleCI isn't going to load
+        them.
+        """
+        self.testimonial = Testimonial(
+            quote_text='Testimonial Quote 1',
+            author='Author 1',
+            quote_text_full='Full Testimonial Text 1.')
+        self.testimonial.save()
+
+        Testimonial.objects.create(
+            quote_text='Testimonial Quote 2',
+            author='Author 2',
+            quote_text_full='Full Testimonial Text 2.')
+        Testimonial.objects.create(
+            quote_text='Testimonial Quote 3',
+            author='Author 3',
+            quote_text_full='Full Testimonial Text 3.')
 
     def test_index(self):
         """Checks that the view is accessible in HTTP"""
-
         response = self.client.get(
-            reverse('testimonial-view', kwargs={'pk': 1}))
+            reverse('testimonial-view', kwargs={'pk': self.testimonial.pk}))
 
         self.assertEqual(response.status_code, 200)
 
@@ -21,12 +37,12 @@ class TestimonialTestCase(TestCase):
         """Check that a testimonial is displayed with correct data."""
 
         response = self.client.get(
-            reverse('testimonial-view', kwargs={'pk': 1}))
+            reverse('testimonial-view', kwargs={'pk': self.testimonial.pk}))
 
         testimonial = response.context['testimonial']
 
-        self.assertEquals(testimonial.pk, 1)
+        self.assertEquals(testimonial.pk, self.testimonial.pk)
         self.assertEquals(
                 testimonial.quote_text, "Testimonial Quote 1")
         self.assertEquals(
-                testimonial.quote_text_full, "Full Testimonial Text 1")
+                testimonial.quote_text_full, "Full Testimonial Text 1.")
