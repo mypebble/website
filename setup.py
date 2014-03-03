5fb59d6c50870293f0c6b8a08b11b41a2347e8c5
Scott Walton <scott.walton@mypebble.co.uk>
2014-03-03 10:17:59 +0000

    Removed install_requires because we'll use requirements.txt

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mypebble',
-    version='1.0.0',
+    version='1.0.1',
     description="MyPebble website",
     author="SF Software limited t/a Pebble",
     author_email="sysadmin@talktopebble.co.uk",
@@ -10,30 +10,4 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
-    install_requires=[
-        'psycopg2>=2.4.2',
-
-        'gunicorn==0.14.3',
-
-        'html5lib==0.95',
-
-        'Django==1.4',
-        'South==0.7.5',
-
-        'django-cms==2.3',
-        'django-mptt==0.5.1',
-        'django-forms-builder==0.7.10',
-
-        'django-mptt==0.5.1',
-        'django-picklefield==0.2.1',
-        'django-activelink==0.3',
-        'django-classy-tags==0.3.4.1',
-        'django-reversion==1.6.1',
-        'django-coverage==1.2.2',
-        'django_logtail==0.0.2',
-
-        'cmsplugin_blog==1.1.2',
-
-        'cmsplugin-randomquote==0.1.3',
-    ],
 )
